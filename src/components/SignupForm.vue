651aac08d6baaed4a40d1ffbe387eff8d453a467
rbxMatrix <rbx.matrix@gmail.com>
2025-04-29 14:52:36 +0200

    Initial Comit

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,90 @@
+<template>
+    <form >
+        <label>Email:</label>
+        <input type="email" required v-model="email">
+
+        <label>Password:</label>
+        <input type="password" required v-model="password">     
+        
+        <label>Role:</label>
+        <select v-model="role">
+            <option value="developper">Web Developper</option>
+            <option value="designer">Web Designer</option>
+        </select>
+        <div class="term">
+            <input type="checkbox" v-model="terms" required>
+            <label >Accept terms and condition</label>
+        </div>
+        <div>
+            <input type="checkbox" value= shaun v-model="names">
+            <label>shaun</label>
+        </div>
+        <div>
+            <input type="checkbox" value= yoshi v-model="names">
+            <label>yoshi</label>
+        </div>
+        <div>
+            <input type="checkbox" value= mario v-model="names">
+            <label>mario</label>
+        </div>
+
+    </form>
+    <p>Email: {{ email }}</p>
+    <p>Password:{{ password }}</p>
+    <p>Role: {{ role }} </p>
+    <p>Terms accepted: {{ terms }}</p>
+    <p>Names: {{ names }}</p>
+</template>
+
+<script>
+export default {
+    data(){
+    return{
+      email:'',
+      password:'',
+      role:'designer',
+      terms: false,
+      names:[]
+    }
+
+  }
+
+}
+</script>
+
+<style>
+    form{
+        max-width: 420px;
+        margin: 30px;
+        background: white;
+        text-align: left;
+        padding: 40px;
+        border-radius: 10px;
+    }
+    label{
+        color:#aaa;
+        display: inline-block;
+        margin:25px 0 15 px;
+        font-size:0.6em;
+        text-transform: uppercase;
+        letter-spacing: 1px;
+        font-weight: bold;
+    }
+    input, select{
+        display: block;
+        padding: 10px 6px;
+        width: 100%;
+        box-sizing:border-box;
+        border: none;
+        border-bottom: 1px solid #ddd;
+        color:#555;
+    }
+    input[type="checkbox"]{
+        display: inline-block;
+        width:16px;
+        margin:0 10px 0 0;
+        position:relative;
+        top: 2px;
+    }
+
+</style>
